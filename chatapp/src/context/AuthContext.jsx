72fac5046fc56e4fd04018389215f6348c2a7345
24f2000880 <your-new-github-email@gmail.com>
2026-06-03 11:46:07 +0530

    Debug Vercel env

--- chatapp/src/context/AuthContext.jsx
+++ chatapp/src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 const AuthContext = createContext();
 
 const API = import.meta.env.VITE_API_URL;
-
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
